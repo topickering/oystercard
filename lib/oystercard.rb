c1bec3b1934ef11f462cd3918cb1a922b17872f4
topickering <topickering@gmail.com>
2018-11-05 15:44:44 +0000

    Commit 7: added maximum balance for card

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,14 +3,21 @@ class Oystercard
   attr_reader :balance
 
   DEFAULT_BALANCE = 0
+  CARD_LIMIT = 90
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
 
   def top_up(amount)
+    fail "Cannot exceed maximum of £#{CARD_LIMIT}" if exceeds_limit?(amount)
     @balance += amount
   end
 
+  private
+
+  def exceeds_limit?(amount)
+    (@balance + amount) > CARD_LIMIT
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,6 +10,11 @@ describe Oystercard do
     it 'allows a user to increase the balance on their oystercard' do
       expect { subject.top_up(10) }.to change{ subject.balance }.by 10
     end
+    it 'will not allow balance to exceed a maximum' do
+      card_limit = Oystercard::CARD_LIMIT
+      subject.top_up(card_limit)
+      expect { subject.top_up(1) }.to raise_error "Cannot exceed maximum of £#{card_limit}"
+    end
   end
 
 
